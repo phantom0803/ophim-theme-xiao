a2689d00dabe8f2f1154a0bbf1f39396423ef116
Hades <opdlnf01@gmail.com>
2022-11-02 23:37:08 +0700

    Get catalog from database

--- resources/views/themexiao/inc/sidebar.blade.php
+++ resources/views/themexiao/inc/sidebar.blade.php
@@ -41,51 +41,53 @@ $tops = Cache::remember('site.movies.tops', setting('site_cache_ttl', 5 * 60), f
                     </div>
                 </div>
                 <div class="myui-panel_bd">
-                    <ul class="myui-vodlist__media active col-pd clearfix">
-                        <li>
-                            <div class="thumb"><a class="myui-vodlist__thumb img-xs-70 lazyload"
-                                    href="{{ $top['data']->first()->getUrl() }}"
-                                    title="{{ $top['data']->first()->name }}"
-                                    data-original="{{ $top['data']->first()->thumb_url }}"></a></div>
-                            <div class="detail detail-side">
-                                <h4 class="title"><a href="{{ $top['data']->first()->getUrl() }}"><i
-                                            class="fa fa-angle-right text-muted pull-right"></i>{{ $top['data']->first()->name }}</a>
-                                </h4>
-                                <p class="font-12"><span
-                                        class="text-muted">{{ $top['data']->first()->origin_name }}</span><span
-                                        class="text-muted"> ({{ $top['data']->first()->publish_year }})</span></p>
-                                <p class="font-12 margin-0">
-                                    <span class="text-muted"><i class="fa fa-eye"></i>
-                                    </span>{{ $top['data']->first()->view_week }}
-                                    <span class="text-muted"><i class="fa fa-star"></i>
-                                    </span>{{ number_format($top['data']->first()->rating_star ?? 0, 1) }}
-                                </p>
-                            </div>
-                        </li>
-                    </ul>
-                    <ul class="myui-vodlist__text col-pd clearfix">
-                        @foreach ($top['data'] as $key => $movie)
-                            @php
-                                switch ($key) {
-                                    case 0:
-                                        $class_top = 'badge-first';
-                                        break;
-                                    case 1:
-                                        $class_top = 'badge-second';
-                                        break;
-                                    case 2:
-                                        $class_top = 'badge-third';
-                                        break;
-                                    default:
-                                        $class_top = '';
-                                        break;
-                                }
-                            @endphp
+                    @if (count($top['data']))
+                        <ul class="myui-vodlist__media active col-pd clearfix">
                             <li>
-                                <a href="{{ $movie->getUrl() }}" title="{{ $movie->name }}"><span class="badge {{ $class_top }}">{{ $key + 1 }}</span>{{ $movie->name }}</a>
+                                <div class="thumb"><a class="myui-vodlist__thumb img-xs-70 lazyload"
+                                        href="{{ $top['data']->first()->getUrl() }}"
+                                        title="{{ $top['data']->first()->name }}"
+                                        data-original="{{ $top['data']->first()->thumb_url }}"></a></div>
+                                <div class="detail detail-side">
+                                    <h4 class="title"><a href="{{ $top['data']->first()->getUrl() }}"><i
+                                                class="fa fa-angle-right text-muted pull-right"></i>{{ $top['data']->first()->name }}</a>
+                                    </h4>
+                                    <p class="font-12"><span
+                                            class="text-muted">{{ $top['data']->first()->origin_name }}</span><span
+                                            class="text-muted"> ({{ $top['data']->first()->publish_year }})</span></p>
+                                    <p class="font-12 margin-0">
+                                        <span class="text-muted"><i class="fa fa-eye"></i>
+                                        </span>{{ $top['data']->first()->view_week }}
+                                        <span class="text-muted"><i class="fa fa-star"></i>
+                                        </span>{{ number_format($top['data']->first()->rating_star ?? 0, 1) }}
+                                    </p>
+                                </div>
                             </li>
-                        @endforeach
-                    </ul>
+                        </ul>
+                        <ul class="myui-vodlist__text col-pd clearfix">
+                            @foreach ($top['data'] as $key => $movie)
+                                @php
+                                    switch ($key) {
+                                        case 0:
+                                            $class_top = 'badge-first';
+                                            break;
+                                        case 1:
+                                            $class_top = 'badge-second';
+                                            break;
+                                        case 2:
+                                            $class_top = 'badge-third';
+                                            break;
+                                        default:
+                                            $class_top = '';
+                                            break;
+                                    }
+                                @endphp
+                                <li>
+                                    <a href="{{ $movie->getUrl() }}" title="{{ $movie->name }}"><span class="badge {{ $class_top }}">{{ $key + 1 }}</span>{{ $movie->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
                 </div>
             @if($page == 'index') </div> @endif
         @endforeach
